3bb5d0cf7230daa0aff6f518298fd5ce6d01d965
성혜람 <wodnr624@gmail.com>
2026-02-11 23:38:35 +0900

    first commit

---
+++ src/main/java/com/oncare/oncare24/Oncare24Application.java
@@ -0,0 +1,13 @@
+package com.oncare.oncare24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oncare24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oncare24Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/oncare/oncare24/Oncare24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oncare.oncare24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oncare24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
